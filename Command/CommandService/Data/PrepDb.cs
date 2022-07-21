f9d93fe93eec70fa08205589082b21dd625cc046
Altairseven <elpiole@gmail.com>
2022-07-21 07:37:27 -0300

    Update PrepDb.cs

--- Command/CommandService/Data/PrepDb.cs
+++ Command/CommandService/Data/PrepDb.cs
@@ -12,11 +12,7 @@ public static class PrepDb {
 
         var platforms = grpcClient!.ReturnAllPlatforms();
 
-
         SeedData(serviceScope!.ServiceProvider.GetService<ICommandRepo>()!, platforms);
-
-
-
     }
 
     private static void SeedData(ICommandRepo _repo, IEnumerable<Platform> platforms) {
